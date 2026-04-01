866eebd4257cad056814d994e13eeaa6f865c925
dongyeul <rlaehdduf04@gmail.com>
2026-04-01 12:02:26 +0900

    fix: use relative paths for navigation links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rlaehdduf.github.io',
+  base: '/portfolio',
   output: 'static',
 });
